c952c348dec4ad5f1156c857113e1df3802a2b06
masud4657 <masudpfrnt@gmail.com>
2024-05-12 11:26:24 +0600

    adeed the get operaton of CRUD for finding services.

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ async function run() {
         res.send(result)
 
     })
+
+    app.get('/addServices',async(req,res)=>{
+      const cursor = serviceCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
