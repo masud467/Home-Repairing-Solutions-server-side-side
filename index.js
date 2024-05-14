f86f71f7a6dee86e00724cbd8b40a5c796dac7f1
masud4657 <masudpfrnt@gmail.com>
2024-05-14 11:59:02 +0600

    I have used get CRUD operation to find specific user's booked  services from purchaseCollection of MongoDb.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const app = express()
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const port = process.env.PORT || 6003
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 6003;
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uoysey8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,86 +25,98 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db("serviceDB").collection("service")
-    const purchaseCollection = client.db("serviceDB").collection("purchase")
+    const serviceCollection = client.db("serviceDB").collection("service");
+    const purchaseCollection = client.db("serviceDB").collection("purchase");
 
-    app.post('/addServices',async(req,res)=>{
-        const newServices = req.body
-        console.log(newServices)
-        const result = await serviceCollection.insertOne(newServices)
-        res.send(result)
+    app.post("/addServices", async (req, res) => {
+      const newServices = req.body;
+      console.log(newServices);
+      const result = await serviceCollection.insertOne(newServices);
+      res.send(result);
+    });
 
-    })
+    app.get("/addServices", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/addServices',async(req,res)=>{
-      const cursor = serviceCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/viewDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/viewDetails/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id:new ObjectId(id)}
-      const result = await serviceCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/purchase/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id:new ObjectId(id)}
-      const result = await serviceCollection.findOne(query)
-      res.send(result)
-    })
-
-    app.post('/purchase',async(req,res)=>{
-      const bookService = req.body
+    app.post("/purchase", async (req, res) => {
+      const bookService = req.body;
       // console.log(bookService)
-      const result = await purchaseCollection.insertOne(bookService)
-      res.send(result)
-    })
+      const result = await purchaseCollection.insertOne(bookService);
+      res.send(result);
+    });
 
-    app.get('/manageService/:providerEmail',async(req,res)=>{
-  
-      console.log(req.params.email)
-    const result = await serviceCollection.find({email:req.params.email}).toArray()
-      res.send(result)
-    })
+    app.get("/manageService/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { providerEmail: email };
+      const result = await serviceCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.delete('/addServices/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id:new ObjectId(id)}
-      const result = await serviceCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/addServices/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
 
-    app.get('/update/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id:new ObjectId(id)}
-      const result = await serviceCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.put('/update/:id',async(req,res)=>{
-      const id = req.params.id
-      const filter = {_id:new ObjectId(id)}
-      const options = { upsert: true }
-      const updateService = req.body
-      const service ={
-        $set:{
-          image:updateService.image,
-          name:updateService.name,
-          price:updateService.price,
-          area:updateService.area,
-          description:updateService.description,
-        }
-      }
-      const result = await serviceCollection.updateOne(filter,service,options)
-      res.send(result)
-    })
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateService = req.body;
+      const service = {
+        $set: {
+          image: updateService.image,
+          name: updateService.name,
+          price: updateService.price,
+          area: updateService.area,
+          description: updateService.description,
+        },
+      };
+      const result = await serviceCollection.updateOne(
+        filter,
+        service,
+        options
+      );
+      res.send(result);
+    });
+
+    app.get("/bookedService/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await purchaseCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -116,12 +124,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Home repairing solutions is running");
+});
 
-
-app.get('/',(req,res)=>{
-    res.send('Home repairing solutions is running')
-} )
-
-app.listen(port,()=>{
-    console.log(`Home repairing solutions is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Home repairing solutions is running on port ${port}`);
+});
